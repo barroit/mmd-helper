1d6836cfcf2c22e61ad931c57fb0e4d5caa92a31
Jiamu Sun <39@barroit.sh>
2026-06-14 13:13:58 +0900

    setup-flatpak-blender.sh: enable discord IPC socket access
    Signed-off-by: Jiamu Sun <39@barroit.sh>

--- setup-flatpak-blender.sh
+++ setup-flatpak-blender.sh
@@ -9,6 +9,7 @@ id=org.blender.Blender
 
 flatpak override --user --reset $id
 flatpak override --user --filesystem=$PWD/blender:ro $id
+flatpak override --user --filesystem=xdg-run/discord-ipc-0 $id
 flatpak run $id --version >.tmp-$$
 
 config=$(flatpak run --command=sh $id -c 'printf %s $XDG_CONFIG_HOME')
