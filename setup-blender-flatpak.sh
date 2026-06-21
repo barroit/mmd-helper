397ade2fe6f8f5f06fcb1d8dc6b551b8fa9546fa
Jiamu Sun <39@barroit.sh>
2026-06-21 15:15:55 +0900

    setup-blender-flatpak.sh: cleanup
    Signed-off-by: Jiamu Sun <39@barroit.sh>

--- setup-blender-flatpak.sh
+++ setup-blender-flatpak.sh
@@ -3,14 +3,11 @@
 
 set -s
 
-trap 'rm -f .tmp-$$' EXIT
-
 id=org.blender.Blender
 
 flatpak override --user --reset $id
 flatpak override --user --filesystem=$PWD/blender:ro $id
 flatpak override --user --filesystem=xdg-run/discord-ipc-0 $id
-flatpak run $id --version >.tmp-$$
 
 ./blender/ln-scripts.sh $(flatpak run --command=sh $id \
 				      -c 'printf %s/blender $XDG_CONFIG_HOME')
